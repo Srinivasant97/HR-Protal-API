afe94e2debc9599dab4f257d9f24ad304b010b7a
vishnu <vishnuv@linarc.io>
2022-05-29 11:59:38 +0530

    db

--- hirarc/settings.py
+++ hirarc/settings.py
@@ -78,27 +78,27 @@ WSGI_APPLICATION = 'hirarc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'd5kat80epnpgdp',
-#         'USER': 'unummtksnuhtkd',
-#         'PASSWORD': '4012967097729ea5c8692b51d84ed96252decb8e2bb81481a3c9a5294597fc87',
-#         'HOST': 'ec2-54-165-90-230.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hirarc',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5433',
+        'NAME': 'd5kat80epnpgdp',
+        'USER': 'unummtksnuhtkd',
+        'PASSWORD': '4012967097729ea5c8692b51d84ed96252decb8e2bb81481a3c9a5294597fc87',
+        'HOST': 'ec2-54-165-90-230.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'hirarc',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5433',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
