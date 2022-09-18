2cba24e4cbb6af469ac3fc003bac6f86915c10ae
Srinivasan T <srinivasant@linarc.io>
2022-09-18 20:06:07 +0530

    Final

--- bot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from .views import AnswerQuestions
-
-urlpatterns = [
-    path('quesions/',AnswerQuestions.as_view(),name='answer')
-]
